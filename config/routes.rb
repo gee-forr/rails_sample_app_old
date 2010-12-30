512f917bf7f970a5717c906d548458f00c642028
Gabriel Fortuna <gee.forr@gmail.com>
2010-12-30 04:41:50 -0800

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ SampleApp::Application.routes.draw do
   get "pages/about"
   get "pages/help"
 
+  get "users/new"
+
+  root              :to => 'pages#home'
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
