06df3d7076da493d799a774cc386e6a087822b18
Gabriel Fortuna <gee.forr@gmail.com>
2011-01-17 11:50:05 -0800

    Finished section 1 of chapter 11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110110190025
+# Schema version: 20110117192150
 #
 # Table name: users
 #
@@ -10,6 +10,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean
 #
 
 require 'digest'
@@ -17,6 +18,8 @@ require 'digest'
 class User < ActiveRecord::Base
   attr_accessor   :password
   attr_accessible :name, :email, :password, :password_confirmation
+
+  has_many :microposts, :dependent => :destroy
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
